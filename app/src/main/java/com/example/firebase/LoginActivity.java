6366a98a0e2f038e60e3b458902f215fd232e6c3
nordin001 <73000909+nordin001@users.noreply.github.com>
2022-10-20 11:00:34 +0200

    Identificacion hecha , Unica problema no muestra la imagen

---
+++ app/src/main/java/com/example/firebase/LoginActivity.java
@@ -0,0 +1,73 @@
+package com.example.firebase;
+
+import android.content.Intent;
+import android.media.MediaDrm;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.firebase.MainActivity;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LoginActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        login();
+    }
+
+    private void login() {
+        FirebaseUser usuario = FirebaseAuth.getInstance().getCurrentUser();
+        if (usuario != null) {
+            Toast.makeText(this, "inicia sesión: " + usuario.getDisplayName() +
+                    " - " + usuario.getEmail(), Toast.LENGTH_LONG).show();
+            Intent i = new Intent(this, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                    | Intent.FLAG_ACTIVITY_NEW_TASK
+                    | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(i);
+        } else {
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.EmailBuilder().build(),
+                    new AuthUI.IdpConfig.GoogleBuilder().build());
+            startActivityForResult(
+                    AuthUI.getInstance().createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .setIsSmartLockEnabled(false)
+                            .build(),
+                    RC_SIGN_IN);
+        }
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode,Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                login();
+                //finish(); //Quitar
+            } else {
+                String s = "";
+                IdpResponse response = IdpResponse.fromResultIntent(data);
+                if (response == null) s = "Cancelado";
+                else switch (response.getError().getErrorCode()) {
+                    //case MediaDrm.ErrorCodes.NO_NETWORK: s="Sin conexión a Internet"; break;
+                    case MediaDrm.ErrorCodes.ERROR_PROVISIONING_CONFIG: s="Error en proveedor"; break;
+                    case MediaDrm.ErrorCodes.ERROR_UNSUPPORTED_OPERATION: s="Error desarrollador"; break;
+                    default: s="Otros errores de autentificación";
+                }
+                Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+}
